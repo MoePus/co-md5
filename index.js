4289d905b5e5101bf1c91978dca6880e6436dc72
Song Wiki <547007249@qq.com>
2017-03-07 13:28:19 +0800

    support other algorithms

--- index.js
+++ index.js
@@ -6,15 +6,35 @@ const CACHESIZE = 4 * 1024 * 1024;//4MB
 /**
  * Hash a Buffer with md5
  *
- * @param {Buffer} [plain]
+ * @param {string} [algorithm] optional options
+ * @param {Buffer,string} [plain]
  * @param {number} [cacheSize] optional options
  * @return {Promise}
  */
-module.exports = function(plain,cacheSize)
+let fn = function(algorithm,plain,cacheSize)
 {
+    if(typeof algorithm === "string")
+    {
+        if(typeof plain === "string")
+        {
+            plain = Buffer.from(plain);
+        }else if(plain === undefined || typeof plain === 'number')
+        {
+            cacheSize = plain;
+            plain = Buffer.from(algorithm);
+            algorithm = 'md5';
+        }
+    }else
+    {
+        cacheSize = plain;
+        plain = algorithm;
+        algorithm = 'md5';
+    }
+    if(!plain)
+        throw new Error('No data provided');
     if(!cacheSize)
         cacheSize = CACHESIZE;
-    let M = crypto.createHash('md5');
+    let M = crypto.createHash(algorithm);
     let Q = co(function*(){
         let len = plain.length;
         let processedLen = 0;
@@ -37,3 +57,11 @@ module.exports = function(plain,cacheSize)
     });
     return Q;
 };
+module.exports = new Proxy(fn,{get(target,name)
+{
+    return function()
+    {
+        target = target.bind(this,name);
+        return target.apply(this,arguments)
+    };
+}});
--- tests/test.js
+++ tests/test.js
@@ -4,10 +4,30 @@
 const md5 = require("../index");
 const crypto = require('crypto')
 
-const buf = Buffer.from('bufferBUFFERbuffer');
+TESTSTR = 'bufferBUFFERbuffer';
+const buf = Buffer.from(TESTSTR);
 
-let Q = md5(buf,2);
+md5(buf).then(val=>{console.log("Co-MD5 result1:",val)});
+md5(buf,2).then(val=>{console.log("Co-MD5 result2:",val)});
+md5(TESTSTR).then(val=>{console.log("Co-MD5 result3:",val)});
+md5(TESTSTR,2).then(val=>{console.log("Co-MD5 result4:",val)});
+md5("md5",buf).then(val=>{console.log("Co-MD5 result5:",val)});
+md5("md5",buf,2).then(val=>{console.log("Co-MD5 result6:",val)});
+md5("md5",TESTSTR).then(val=>{console.log("Co-MD5 result7:",val)});
+md5("md5",TESTSTR,2).then(val=>{console.log("Co-MD5 result8:",val)});
+md5.md5(TESTSTR,2).then(val=>{console.log("Co-MD5 result9:",val)});
+md5.sha1(TESTSTR,2).then(val=>{console.log("Co-MD5 sha-1 result:",val)});
+md5.sha256(TESTSTR,2).then(val=>{console.log("Co-MD5 sha-256 result:",val)});
 
-Q.then(val=>{console.log("Co-MD5 result: ",val)});
 
-console.log("MD5 result: ",crypto.createHash('md5').update(buf).digest('hex'));
+try
+{
+    md5.none(TESTSTR,2).then(val=>{console.log("Co-MD5 none result:",val)});
+}
+catch(err)
+{
+    console.error("Co-MD5 err:",err.message);
+}
+
+
+console.log("MD5 result:",crypto.createHash('md5').update(buf).digest('hex'));
